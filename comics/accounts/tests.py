6b7eb9b5c2a850b5eee0fc915fae241babb1f02b
datagutten <datagutten@datagutten.net>
2018-12-22 09:37:22 +0100

    Updated accounts tests to work with allauth

--- comics/accounts/tests.py
+++ comics/accounts/tests.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
+from allauth.account.models import EmailAddress
 
 
 def create_user():
-    return User.objects.create_user('alice', 'alice@example.com', 'secret')
+    user = User.objects.create_user('alice', 'alice@example.com', 'secret')
+    EmailAddress(user=user, verified=True, primary=True).save()
+    return user
 
 
 class LoginTest(TestCase):
@@ -17,28 +20,28 @@ class LoginTest(TestCase):
 
         self.assertEquals(response.status_code, 302)
         self.assertEquals(
-            response['Location'], '/account/login/?next=/')
+            response['Location'], '/accounts/login/?next=/')
 
     def test_login_page_includes_email_and_password_fields(self):
-        response = self.client.get('/account/login/')
+        response = self.client.get('/accounts/login/')
 
         self.assertEquals(response.status_code, 200)
-        self.assertIn('Email', response.content)
+        self.assertIn('E-mail', response.content)
         self.assertIn('Password', response.content)
 
     def test_successful_login_redirects_to_front_page(self):
         response = self.client.post(
-            '/account/login/',
-            {'email': 'alice@example.com', 'password': 'secret'})
+            '/accounts/login/',
+            {'login': 'alice@example.com', 'password': 'secret'})
 
         self.assertEquals(response.status_code, 302)
         self.assertEquals(response['Location'], '/')
 
     def test_failed_login_shows_error_on_login_page(self):
         response = self.client.post(
-            '/account/login/',
-            {'email': 'alice@example.com', 'password': 'wrong'})
+            '/accounts/login/',
+            {'login': 'alice@example.com', 'password': 'wrong'})
 
         self.assertEquals(response.status_code, 200)
         self.assertIn(
-            'Please enter a correct username and password.', response.content)
+            'The e-mail address and/or password you specified are not correct.', response.content)
