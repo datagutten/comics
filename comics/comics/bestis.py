a8a33748f1012ecf5421cf991b0f4b2b86b1ed85
datagutten <datagutten@datagutten.net>
2020-05-16 09:39:08 +0200

    "Bestis" is only published on saturdays

--- comics/comics/bestis.py
+++ comics/comics/bestis.py
@@ -12,9 +12,9 @@ class ComicData(ComicDataBase):
 
 
 class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 14
-    schedule = 'Mo,Tu,We,Th,Fr,Sa'
-    time_zone = 'Europe/Oslo'
+    history_capable_days = 30
+    schedule = "Sa"
+    time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
         return self.crawl_helper('bestis', pub_date)
