edf1e2a20de4b42ba53d7c08da9a78dfcdfdfad9
datagutten <datagutten@datagutten.net>
2020-05-16 09:30:56 +0200

    "Fagprat" is no longer published on dagbladet.no

--- comics/comics/fagprat.py
+++ comics/comics/fagprat.py
@@ -5,16 +5,15 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Fagprat (db.no)'
-    language = 'no'
-    url = 'http://www.dagbladet.no/tegneserie/fagprat'
-    rights = 'Flu Hartberg'
+    name = "Fagprat (db.no)"
+    language = "no"
+    url = "http://www.dagbladet.no/tegneserie/fagprat"
+    rights = "Flu Hartberg"
+    active = False
 
 
 class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 14
-    schedule = 'Tu,Th'
-    time_zone = 'Europe/Oslo'
+    time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
         return self.crawl_helper('fagprat', pub_date)
