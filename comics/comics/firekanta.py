773957b4f703d1aa1318d7548402c56e9a99216c
datagutten <datagutten@datagutten.net>
2020-05-16 09:32:08 +0200

    "Firekanta" is no longer published on dagbladet.no

--- comics/comics/firekanta.py
+++ comics/comics/firekanta.py
@@ -5,16 +5,15 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Firekanta'
-    language = 'no'
-    url = 'http://www.dagbladet.no/tegneserie/firekanta'
-    rights = 'Nils Axle Kanten'
+    name = "Firekanta"
+    language = "no"
+    url = "http://www.dagbladet.no/tegneserie/firekanta"
+    rights = "Nils Axle Kanten"
+    active = False
 
 
 class Crawler(DagbladetCrawlerBase):
-    history_capable_days = 14
-    schedule = 'Mo,We,Fr'
-    time_zone = 'Europe/Oslo'
+    time_zone = "Europe/Oslo"
 
     def crawl(self, pub_date):
         return self.crawl_helper('firekanta', pub_date)
