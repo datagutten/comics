cf1378725157de4891eb92607794d2a987de0d98
datagutten <datagutten@datagutten.net>
2020-05-17 16:04:42 +0200

    "Ctrl+Alt+Del Sillies" is no longer active and history is not available

--- comics/comics/ctrlaltdelsillies.py
+++ comics/comics/ctrlaltdelsillies.py
@@ -1,26 +1,16 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Ctrl+Alt+Del Sillies'
-    language = 'en'
-    url = 'http://www.cad-comic.com/sillies/'
-    start_date = '2008-06-27'
-    rights = 'Tim Buckley'
+    name = "Ctrl+Alt+Del Sillies"
+    language = "en"
+    url = "http://www.cad-comic.com/sillies/"
+    start_date = "2008-06-27"
+    rights = "Tim Buckley"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = '2008-06-27'
-    schedule = None
-    time_zone = 'US/Eastern'
-
-    # Without User-Agent set, the server returns empty responses
-    headers = {'User-Agent': 'Mozilla/4.0'}
-
     def crawl(self, pub_date):
-        page = self.parse_page(
-            'http://www.cad-comic.com/sillies/%s' %
-            pub_date.strftime('%Y%m%d'))
-        url = page.src('img[src*="/comics/"]')
-        return CrawlerImage(url)
+        pass
