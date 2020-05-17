64e71c3ee1b78699ec980fbbe10f3bc3574ece70
datagutten <datagutten@datagutten.net>
2020-05-17 21:56:48 +0200

    "A Softer World" is ended

--- comics/comics/asofterworld.py
+++ comics/comics/asofterworld.py
@@ -3,11 +3,12 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'A Softer World'
-    language = 'en'
-    url = 'http://www.asofterworld.com/'
-    start_date = '2003-02-07'
-    rights = 'Joey Comeau, Emily Horne'
+    name = "A Softer World"
+    language = "en"
+    url = "http://www.asofterworld.com/"
+    start_date = "2003-02-07"
+    rights = "Joey Comeau, Emily Horne"
+    active = False
 
 
 class Crawler(CrawlerBase):
