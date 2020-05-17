04574abdeeec9125425691a0869863ad2d04087d
datagutten <datagutten@datagutten.net>
2020-05-17 21:58:01 +0200

    "Completely Serious Comics" is no longer active

--- comics/comics/completelyseriouscomics.py
+++ comics/comics/completelyseriouscomics.py
@@ -3,11 +3,12 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Completely Serious Comics'
-    language = 'en'
-    url = 'http://completelyseriouscomics.com/'
-    start_date = '2010-12-30'
-    rights = 'Jesse'
+    name = "Completely Serious Comics"
+    language = "en"
+    url = "http://completelyseriouscomics.com/"
+    start_date = "2010-12-30"
+    rights = "Jesse"
+    active = False
 
 
 class Crawler(CrawlerBase):
