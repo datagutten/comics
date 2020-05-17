1f2c536fbaf05817af04819619e9adcba97af99e
datagutten <datagutten@datagutten.net>
2020-05-17 21:52:10 +0200

    "Crooked Gremlins" is no longer active

--- comics/comics/crookedgremlins.py
+++ comics/comics/crookedgremlins.py
@@ -3,11 +3,12 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Crooked Gremlins'
-    language = 'en'
-    url = 'http://www.crookedgremlins.com/'
-    start_date = '2008-04-01'
-    rights = 'Carter Fort and Paul Lucci'
+    name = "Crooked Gremlins"
+    language = "en"
+    url = "http://www.crookedgremlins.com/"
+    start_date = "2008-04-01"
+    rights = "Carter Fort and Paul Lucci"
+    active = False
 
 
 class Crawler(CrawlerBase):
