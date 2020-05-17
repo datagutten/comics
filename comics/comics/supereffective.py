8990179d770903a17fc41f86541a13ea65626645
datagutten <datagutten@datagutten.net>
2020-05-17 16:05:30 +0200

    "Super Effective" is no longer active

--- comics/comics/supereffective.py
+++ comics/comics/supereffective.py
@@ -3,11 +3,12 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'Super Effective'
-    language = 'en'
-    url = 'http://www.vgcats.com/super/'
-    start_date = '2008-04-23'
-    rights = 'Scott Ramsoomair'
+    name = "Super Effective"
+    language = "en"
+    url = "http://www.vgcats.com/super/"
+    start_date = "2008-04-23"
+    rights = "Scott Ramsoomair"
+    active = False
 
 
 class Crawler(CrawlerBase):
