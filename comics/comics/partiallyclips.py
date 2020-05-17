f7236752fe8746841d4d0619809547279be16d14
datagutten <datagutten@datagutten.net>
2020-05-17 21:49:49 +0200

    "PartiallyClips" is no longer active and site is gone

--- comics/comics/partiallyclips.py
+++ comics/comics/partiallyclips.py
@@ -1,26 +1,16 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'PartiallyClips'
-    language = 'en'
-    url = 'http://partiallyclips.com/'
-    start_date = '2002-01-01'
-    rights = 'Robert T. Balder'
+    name = "PartiallyClips"
+    language = "en"
+    url = "http://partiallyclips.com/"
+    start_date = "2002-01-01"
+    rights = "Robert T. Balder"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 32
-    schedule = 'Tu'
-    time_zone = 'US/Eastern'
-
     def crawl(self, pub_date):
-        feed = self.parse_feed('http://partiallyclips.com/feed/')
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img')
-            if not url:
-                continue
-            url = url.replace('comics-rss', 'comics')
-            title = entry.title.split(' - ')[0]
-            return CrawlerImage(url, title)
+        pass
