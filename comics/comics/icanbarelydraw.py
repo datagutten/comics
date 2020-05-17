85b8bd7e6ada4795a1292248de2248f621a1886b
datagutten <datagutten@datagutten.net>
2020-05-17 16:08:38 +0200

    "I Can Barely Draw" is no longer active

--- comics/comics/icanbarelydraw.py
+++ comics/comics/icanbarelydraw.py
@@ -3,11 +3,12 @@ from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
-    name = 'I Can Barely Draw'
-    language = 'en'
-    url = 'http://www.icanbarelydraw.com/'
-    start_date = '2011-08-05'
-    rights = 'Group effort, CC BY-NC-ND 3.0'
+    name = "I Can Barely Draw"
+    language = "en"
+    url = "http://www.icanbarelydraw.com/"
+    start_date = "2011-08-05"
+    rights = "Group effort, CC BY-NC-ND 3.0"
+    active = False
 
 
 class Crawler(CrawlerBase):
