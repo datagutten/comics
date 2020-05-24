0b0ce3cabd53df72c5f88c243b159410cdd7b740
datagutten <datagutten@datagutten.net>
2020-05-24 20:11:12 +0200

    "Teh Gladiators" is no longer active Feed exists, but contains only blog posts (no comics)

--- comics/comics/tehgladiators.py
+++ comics/comics/tehgladiators.py
@@ -1,24 +1,18 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
 class ComicData(ComicDataBase):
     name = "Teh Gladiators"
     language = "en"
-    url = "http://www.tehgladiators.com/"
+    url = "http://www.tehgladiators.com/book_cat/teh-gladiator"
     start_date = "2008-03-18"
     rights = "Uros Jojic & Borislav Grabovic"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 90
-    schedule = "We"
     time_zone = "Europe/Belgrade"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.tehgladiators.com/rss.xml")
-        for entry in feed.for_date(pub_date):
-            page = self.parse_page(entry.link)
-            url = page.src('img[alt^="Teh Gladiators Webcomic"]')
-            title = entry.title
-            return CrawlerImage(url, title)
+        pass
