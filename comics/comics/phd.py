907e8c5ffe4b19642859eb27f267fb8ccfb21c13
datagutten <datagutten@datagutten.net>
2020-05-31 09:56:05 +0200

    "Piled Higher and Deeper" is no longer active, but has history

--- comics/comics/phd.py
+++ comics/comics/phd.py
@@ -7,17 +7,24 @@ class ComicData(ComicDataBase):
     language = "en"
     url = "http://www.phdcomics.com/"
     start_date = "1997-10-27"
+    end_date = "2018-12-24"
     rights = "Jorge Cham"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_date = "1997-10-27"
+    history_capable_date = ComicData.start_date
     schedule = None
     time_zone = "US/Pacific"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.phdcomics.com/gradfeed.php")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src("img")
-            title = entry.title.split("'")[1]
-            return CrawlerImage(url, title)
+        page = self.parse_page('http://phdcomics.com/comics/archive_list.php')
+        release = page.root.xpath('//a/b[.="%s"]/../@href' % pub_date.strftime('%-m/%-d/%Y'))
+        if not release:
+            return
+
+        release_page = self.parse_page(release[0])
+        title = release_page.content('meta[name="twitter:title"]')
+        url = release_page.href('link[rel="image_src"]')
+
+        return CrawlerImage(url, title)
