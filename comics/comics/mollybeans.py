dc23286cf1bfe73ed301ffe972441c99d987aab0
datagutten <datagutten@datagutten.net>
2020-06-01 15:07:47 +0200

    "Molly Beans" is no longer active and site is gone

--- comics/comics/mollybeans.py
+++ comics/comics/mollybeans.py
@@ -1,4 +1,4 @@
-from comics.aggregator.crawler import CrawlerBase, CrawlerImage
+from comics.aggregator.crawler import CrawlerBase
 from comics.core.comic_data import ComicDataBase
 
 
@@ -8,19 +8,9 @@ class ComicData(ComicDataBase):
     url = "http://www.mollybeans.com/"
     start_date = "2016-02-11"
     rights = "Dan Sacharow"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 30
-    schedule = "We,Fr"
-    time_zone = "US/Eastern"
-
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.mollybeans.com/feed/")
-        for entry in feed.for_date(pub_date):
-            title = entry.title
-            if "/comic/" not in entry.link:
-                continue
-            page = self.parse_page(entry.link)
-            url = page.src("img.attachment-panel-comic-strip")
-            return CrawlerImage(url, title)
+        pass
