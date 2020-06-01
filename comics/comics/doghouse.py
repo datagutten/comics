e1ab731ab07d2fac0eb31d40c0dd5608569fdf1e
datagutten <datagutten@datagutten.net>
2020-06-01 11:54:36 +0200

    Rewrite "The Doghouse Diaries" after site change

--- comics/comics/doghouse.py
+++ comics/comics/doghouse.py
@@ -1,3 +1,5 @@
+from django.utils.dateformat import DateFormat
+
 from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
@@ -8,19 +10,35 @@ class ComicData(ComicDataBase):
     url = "http://www.thedoghousediaries.com/"
     start_date = "2009-01-08"
     rights = "Will, Ray, & Raf"
+    active = False
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 30
+    history_capable_date = "2009-01-08"
     schedule = "Mo,We,Fr"
     time_zone = "US/Pacific"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed(
-            "http://feeds.feedburner.com/thedoghousediaries/feed"
+        page = self.parse_page("http://thedoghousediaries.com/archive")
+
+        tables = page.root.xpath(
+            '//div[@id="%s"]/following-sibling::table'
+            % pub_date.strftime("%Y")
         )
-        for entry in feed.for_date(pub_date):
-            url = entry.content0.src("img.comic-item")
-            title = entry.content0.alt("img.comic-item")
-            text = entry.content0.title("img.comic-item")
-            return CrawlerImage(url, title, text)
+        if not tables:  # No releases that year
+            return
+
+        table = tables[0]
+        date = DateFormat(pub_date).format("F jS")
+        releases = table.xpath('tr/td[.="%s"]/../td/a' % date)
+        if not releases:  # No release that date
+            return
+
+        images = []
+        for release in releases:
+            title = release.text
+            page = self.parse_page(release.get("href"))
+            url = page.src("div#imgdiv img")
+            images.append(CrawlerImage(url, title))
+
+        return images
