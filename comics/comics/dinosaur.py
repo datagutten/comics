ebb1abfe80defb5b587c585ab723275fd6bb3116
datagutten <datagutten@datagutten.net>
2020-06-01 10:20:48 +0200

    Rewrite "Dinosaur Comics", feed is gone

--- comics/comics/dinosaur.py
+++ comics/comics/dinosaur.py
@@ -2,6 +2,23 @@ from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.core.comic_data import ComicDataBase
 
 
+def make_ordinal(n):
+    """
+    Convert an integer into its ordinal representation::
+
+        make_ordinal(0)   => '0th'
+        make_ordinal(3)   => '3rd'
+        make_ordinal(122) => '122nd'
+        make_ordinal(213) => '213th'
+    https://stackoverflow.com/a/50992575/2630074
+    """
+    n = int(n)
+    suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    if 11 <= (n % 100) <= 13:
+        suffix = "th"
+    return str(n) + suffix
+
+
 class ComicData(ComicDataBase):
     name = "Dinosaur Comics"
     language = "en"
@@ -11,14 +28,26 @@ class ComicData(ComicDataBase):
 
 
 class Crawler(CrawlerBase):
-    history_capable_days = 32
+    history_capable_date = "2003-02-01"
     schedule = "Mo,Tu,We,Th"
     time_zone = "US/Eastern"
 
     def crawl(self, pub_date):
-        feed = self.parse_feed("http://www.rsspect.com/rss/qwantz.xml")
-        for entry in feed.for_date(pub_date):
-            url = entry.summary.src('img[src*="/comics/"]')
-            title = entry.title
-            text = entry.summary.title('img[src*="/comics/"]')
-            return CrawlerImage(url, title, text)
+        page = self.parse_page("http://www.qwantz.com/archive.php")
+        # Format the date in the form "January 31st, 2020"
+        date = "%s %s, %s" % (
+            pub_date.strftime("%B"),
+            make_ordinal(pub_date.strftime("%d")),
+            pub_date.strftime("%Y"),
+        )
+        link = page.root.xpath('//a[.="%s"]' % date)
+
+        if not link:
+            return
+        title = link[0].tail
+        page_url = link[0].get("href")
+        page = self.parse_page(page_url)
+        url = page.src('img[src*="comics"]')
+        text = page.title('img[src*="comics"]')
+
+        return CrawlerImage(url, title, text)
